a238291b8b7cbd147375d45f353881eda5c89301
Warlocks1507Programmer <60792102+Warlocks1507Programmer@users.noreply.github.com>
2026-02-05 19:38:57 -0800

    Added Creep

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import com.ctre.phoenix6.hardware.TalonFXS;
 // WPI libraries
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -56,12 +57,14 @@ import frc.robot.auto.routines.AutoSample;
 import static frc.robot.Constants.Speed.*;
 import static frc.robot.Constants.Shooter.*;
 
+import frc.robot.generated.TunerConstants;
+
 import static edu.wpi.first.units.Units.*;
 public class RobotContainer {
     private final SendableChooser<Command> autoChooser = new SendableChooser<>();
     
-    private double MaxSpeed = 0.3 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    public static double MaxSpeed = 0.5 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    public static double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
@@ -209,7 +212,17 @@ public class RobotContainer {
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // Reset the field-centric heading on left bumper press.
-        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+        joystick.y().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+
+
+        joystick.leftBumper().whileTrue(new InstantCommand(() -> {
+            MaxSpeed = 0.2 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+            MaxAngularRate = RotationsPerSecond.of(0.50).in(RadiansPerSecond);
+        }));
+        joystick.leftBumper().whileFalse(new InstantCommand(() -> {
+            MaxSpeed = 0.75 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+            MaxAngularRate = RotationsPerSecond.of(.75).in(RadiansPerSecond);
+        }));
         
 
         // ---------------------------------
