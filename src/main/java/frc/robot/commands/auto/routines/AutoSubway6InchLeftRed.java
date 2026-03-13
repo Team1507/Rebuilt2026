1b16d0f917683baa541ce77ad62704efc68da6db
Warlocks1507Programmer <60792102+Warlocks1507Programmer@users.noreply.github.com>
2026-03-13 16:39:45 -0400

    Auto Left RED

--- src/main/java/frc/robot/commands/auto/routines/AutoSubway6InchLeftRed.java
+++ src/main/java/frc/robot/commands/auto/routines/AutoSubway6InchLeftRed.java
@@ -1,5 +1,49 @@
+//  ██╗    ██╗ █████╗ ██████╗ ██╗      ██████╗  ██████╗██╗  ██╗███████╗
+//  ██║    ██║██╔══██╗██╔══██╗██║     ██╔═══██╗██╔════╝██║ ██╔╝██╔════╝
+//  ██║ █╗ ██║███████║██████╔╝██║     ██║   ██║██║     █████╔╝ ███████╗
+//  ██║███╗██║██╔══██║██╔══██╗██║     ██║   ██║██║     ██╔═██╗ ╚════██║
+//  ╚███╔███╔╝██║  ██║██║  ██║███████╗╚██████╔╝╚██████╗██║  ██╗███████║
+//   ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝
+//                           TEAM 1507 WARLOCKS
+//                         Programmed by Andrew :)
 package frc.robot.commands.auto.routines;
 
+
+
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.kShooter;
+import frc.robot.commands.auto.AutoSequence;
+import frc.robot.framework.CoordinatorRecord;
+import frc.robot.framework.SubsystemsRecord;
+import frc.robot.localization.nodes.Nodes;
+
+
 public class AutoSubway6InchLeftRed {
-    
+    public static Command build(SubsystemsRecord record, CoordinatorRecord coordinator, double MaxSpeed, double MaxAngularRate) {
+
+
+        return new AutoSequence(record, coordinator, MaxSpeed, MaxAngularRate)
+            .startTimer()
+            .resetPose(Nodes.Start.Blue.LEFT)
+            .moveThrough(Nodes.Start.Red.LEFT_OVER_BUMP, 0.2)
+            .intakeHigh()
+            .parallel(
+                seq -> seq.withSpeed(MaxSpeed * 0.5).moveThrough(Nodes.Midfield.LEFT_LEFT_SUBWAY_RED, 0.1),
+                seq -> seq.intakeDeploy())
+            .withSpeed(MaxSpeed * 0.5).moveThrough(Nodes.Midfield.RIGHT_LEFT_SUBWAY_RED, 0.5)
+
+
+            .intakeHigh()
+            .parallel(
+                seq -> seq.intakeRetract(),
+                seq -> seq.moveThrough(Nodes.Start.Red.LEFT_BEFORE_BUMP, 0.5))  
+            .moveThrough(Nodes.Start.Red.LEFT_OVER_BUMP, 0.2)
+            .driveTo(Nodes.Start.Blue.LEFT)
+            .waitSeconds(0.5)
+            .pointToShoot()
+            .shootUntil(19.99)
+            .build();
+    }
 }
--- src/main/java/frc/robot/localization/nodes/Nodes.java
+++ src/main/java/frc/robot/localization/nodes/Nodes.java
@@ -218,9 +218,9 @@ public final class Nodes {
         public static final Pose2d MIDDLE_18INCH_SUBWAY_RED =
                 new Pose2d(7.75,4.0,Rotation2d.fromDegrees(90.0));
         public static final Pose2d RIGHT_LEFT_SUBWAY_RED =
-                new Pose2d(7.75, 5.0, Rotation2d.fromDegrees(270.0));
+                new Pose2d(8.76, 3.04, Rotation2d.fromDegrees(270.0));
         public static final Pose2d LEFT_LEFT_SUBWAY_RED =
-                new Pose2d(7.75, 7.4, Rotation2d.fromDegrees(270.0));
+                new Pose2d(8.81, 0.64, Rotation2d.fromDegrees(270.0));
         public static final Pose2d DOUBLE_LEFT_SUBWAY_RED =
                 new Pose2d(7.75, 7.4, Rotation2d.fromDegrees(90.0));
         public static final Pose2d LEFT_FOOTLONG_SUBWAY_RED =
@@ -247,9 +247,9 @@ public final class Nodes {
 
                public static final class Red{
                public static final Pose2d RIGHT_OVER_BUMP =
-                new Pose2d(10.7,5.3, Rotation2d.fromDegrees(225.0));
+                new Pose2d(10.7,5.7, Rotation2d.fromDegrees(225.0));
         public static final Pose2d RIGHT_BEFORE_BUMP =
-                new Pose2d(9.8,5.3, Rotation2d.fromDegrees(170.0));
+                new Pose2d(9.8,5.7, Rotation2d.fromDegrees(170.0));
                }
     }
 
@@ -265,6 +265,10 @@ public final class Nodes {
                         new Pose2d(2.000, 4.115, Rotation2d.fromDegrees(0.0));
                 public static final Pose2d RIGHT =
                         new Pose2d(12.8,5.3, Rotation2d.fromDegrees(225.0));
+                public static final Pose2d LEFT_OVER_BUMP =
+                        new Pose2d(10.21,2.44, Rotation2d.fromDegrees(315.0));
+                public static final Pose2d LEFT_BEFORE_BUMP =
+                        new Pose2d(9.01,2.44, Rotation2d.fromDegrees(315.0));
 
                  
         }
@@ -279,7 +283,9 @@ public final class Nodes {
                         new Pose2d(3.5,2.5, Rotation2d.fromDegrees(55.0));
 
                 public static final Pose2d RIGHT_RED =
-                        new Pose2d(11.5,5.5, Rotation2d.fromDegrees(55.0));
+                        new Pose2d(11.5,5.7, Rotation2d.fromDegrees(55.0));
+                public static final Pose2d LEFT_RED =
+                        new Pose2d(13.01,8.04,Rotation2d.fromDegrees(315.0));
         }
     }
     
